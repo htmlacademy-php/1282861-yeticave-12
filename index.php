32b4e9369ab445a6db2b9f059b14e0284e84d553
Sergey Komlev <sn_komlev@mail.ru>
2020-11-25 17:41:09 +0300

    Заменил цикл на foreach

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
-    <link href="../1282861-yeticave-12/css/normalize.min.css" rel="stylesheet">
-    <link href="../1282861-yeticave-12/css/style.css" rel="stylesheet">
+    <link href="css/normalize.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
@@ -18,7 +18,7 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
     <div class="main-header__container container">
         <h1 class="visually-hidden">YetiCave</h1>
         <a class="main-header__logo">
-            <img src="../1282861-yeticave-12/img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+            <img src="img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
         </a>
         <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
             <label>
@@ -26,13 +26,13 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
             </label>
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
-        <a class="main-header__add-lot button" href="../1282861-yeticave-12/pages/add-lot.html">Добавить лот</a>
+        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-        <?php   if ($is_auth == 1): ?>
+        <?php if ($is_auth == 1): ?>
             <div class="user-menu__logged">
                 <p>Sergey</p>
-                <a class="user-menu__bets" href="../1282861-yeticave-12/pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
         <?php   else: ?>
@@ -57,19 +57,21 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <?php
-            $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+            $categories = [
+                "boards" => "Доски и лыжи",
+                "binding" => "Крепления",
+                "boots" => "Ботинки",
+                "clothes" => "Одежда",
+                "tools" => "Инструменты",
+                "other" => "Разное"
+            ];
             ?>
-
-            <?php
-            $index = 0;
-            $num = count($categories);
-            while ($index < $num): ?>
+            <?php foreach ($categories as $key => $category): ?>
             <!--заполните этот список из массива категорий-->
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="../1282861-yeticave-12/pages/all-lots.html"><?=$categories[$index];?></a>
-                <?php $index++; ?>
-                <?php endwhile; ?>
+                <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -83,37 +85,32 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
                     "title" => "2014 Rossignol District Snowboard",
                     "categories" => "Доски и лыжи",
                     "price" => 10999,
-                    "image" => "../1282861-yeticave-12/img/lot-1.jpg"
-                ],
-                [
+                    "image" => "lot-1.jpg"
+                ],[
                     "title" => "DC Ply Mens 2016/2017 Snowboard",
                     "categories" => "Доски и лыжи",
                     "price" => 15999,
-                    "image" => "../1282861-yeticave-12/img/lot-2.jpg"
-                ],
-                [
+                    "image" => "lot-2.jpg"
+                ],[
                     "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
                     "categories" => "Крепления",
                     "price" => 8000,
-                    "image" => "../1282861-yeticave-12/img/lot-3.jpg"
-                ],
-                [
+                    "image" => "lot-3.jpg"
+                ],[
                     "title" => "Ботинки для сноуборда DC Mutiny Charocal",
                     "categories" => "Ботинки",
                     "price" => 10999,
-                    "image" => "../1282861-yeticave-12/img/lot-4.jpg"
-                ],
-                [
+                    "image" => "lot-4.jpg"
+                ],[
                     "title" => "Куртка для сноуборда DC Mutiny Charocal",
                     "categories" => "Одежда",
                     "price" => 7500,
-                    "image" => "../1282861-yeticave-12/img/lot-5.jpg"
-                ],
-                [
+                    "image" => "lot-5.jpg"
+                ],[
                     "title" => "Маска Oakley Canopy",
                     "categories" => "Разное",
                     "price" => 5400,
-                    "image" => "../1282861-yeticave-12/img/lot-6.jpg"
+                    "image" => "lot-6.jpg"
                 ]
             ];
             ?><!--заполните этот список из массива с товарами-->
@@ -123,7 +120,7 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
                 </div>
                 <div class="lot__info">
                     <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="../1282861-yeticave-12/pages/lot.html">Название товара</a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
@@ -143,17 +140,12 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php
-            $index = 0;
-            $num = count($categories);
-            while ($index < $num): ?>
-
+            <?php foreach ($categories as $key => $category): ?>
             <!--заполните этот список из массива категорий-->
             <li class="nav__item">
-                <a href="../1282861-yeticave-12/pages/all-lots.html"><?=$categories[$index];?></a>
-                <?php $index++; ?>
-                <?php endwhile; ?>
+                <a href="pages/all-lots.html"><?=$category; ?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
@@ -183,7 +175,7 @@ $user_name = 'Sergey'; // укажите здесь ваше имя
                 <svg width="27" height="27" viewBox="0 0 27 27" xmlns="http://www.w3.org/2000/svg"><circle stroke="#879296" fill="none" cx="13.5" cy="13.5" r="12.666"/><path fill="#879296" d="M13.92 18.07c.142-.016.278-.074.39-.166.077-.107.118-.237.116-.37 0 0 0-1.13.516-1.296.517-.165 1.208 1.09 1.95 1.58.276.213.624.314.973.28h1.95s.973-.057.525-.837c-.38-.62-.865-1.17-1.432-1.626-1.208-1.1-1.043-.916.41-2.816.886-1.16 1.236-1.86 1.13-2.163-.108-.302-.76-.214-.76-.214h-2.164c-.092-.026-.19-.026-.282 0-.083.058-.15.135-.195.225-.224.57-.49 1.125-.8 1.656-.973 1.61-1.344 1.697-1.51 1.59-.37-.234-.272-.975-.272-1.433 0-1.56.243-2.202-.468-2.377-.32-.075-.647-.108-.974-.098-.604-.052-1.213.01-1.793.186-.243.116-.438.38-.32.4.245.018.474.13.642.31.152.303.225.638.214.975 0 0 .127 1.832-.302 2.056-.43.223-.692-.167-1.55-1.618-.29-.506-.547-1.03-.77-1.57-.038-.09-.098-.17-.174-.233-.1-.065-.214-.108-.332-.128H6.485s-.312 0-.42.137c-.106.135 0 .36 0 .36.87 2 2.022 3.868 3.42 5.543.923.996 2.21 1.573 3.567 1.598z"/></svg>
             </a>
         </div>
-        <a class="main-footer__add-lot button" href="../1282861-yeticave-12/pages/add-lot.html">Добавить лот</a>
+        <a class="main-footer__add-lot button" href="pages/add-lot.html">Добавить лот</a>
         <div class="main-footer__developed-by">
             <span class="visually-hidden">Разработано:</span>
             <a class="logo-academy" href="https://htmlacademy.ru/intensive/php">
