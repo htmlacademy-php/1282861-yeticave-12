f06a7f3759aa3e68fedc81d745d7025d80bd1c29
Sergey <sn_komlev@mail.ru>
2020-11-28 14:17:58 +0300

    создал функцию форматирования со знаком рубля

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ $user_name = 'Sergey';
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product["title"]; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__amount">Стартовая цена </span>
                             <span class="lot__cost"><?= formatting_sum($product["price"]); ?></span>
                         </div>
                         <div class="lot__timer timer">
