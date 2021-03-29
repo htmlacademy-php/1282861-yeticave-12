20ec36e57aaaed06a217b39de67d8ebd8523db44
Sergey <sn_komlev@mail.ru>
2021-03-29 14:04:33 +0300

    подключился к MySQL,  отправил запросы

--- index.php
+++ index.php
@@ -5,6 +5,20 @@ require_once("helpers.php");
 
 date_default_timezone_set("Europe/Moscow");
 
+$con = mysqli_connect("localhost", "root", "", "yeticave");
+mysqli_set_charset($con, "utf8");
+$sql_products = "SELECT l.id, l.title, l.start_price, l.image, c.name, IFNULL(MAX(b.price), l.start_price) AS price
+FROM lot AS l
+         JOIN category AS c ON c.id = l.category_id
+         LEFT JOIN bet AS b ON b.lot_id = l.id
+WHERE l.expiration_date > NOW()
+GROUP BY l.id";
+$result_products = mysqli_query($con, $sql_products);
+$products = mysqli_fetch_all($result_products, MYSQLI_ASSOC);
+
+$sql_categories = "SELECT name, symbol_code FROM category";
+$result_categories = mysqli_query($con,$sql_categories);
+$categories = mysqli_fetch_all($result_categories, MYSQLI_ASSOC);
 
 $page_content = include_template('main.php', ['categories' => $categories, 'products' => $products]);
 $layout_content = include_template('layout.php', [
@@ -17,3 +31,4 @@ $layout_content = include_template('layout.php', [
 ]);
 
 print($layout_content);
+
